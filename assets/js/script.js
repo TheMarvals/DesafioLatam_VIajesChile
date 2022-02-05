da0dedf628348d082cf51646998b3344b32f5e9d
Eduardo Marval <hello@themarvals.com>
2022-02-04 22:38:21 -0300

    nav-background
    scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+var navbar = document.querySelector('nav')
+
+window.onscroll = function() {
+
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
+}
